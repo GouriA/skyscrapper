4a51894dfcf82c9a89963b4bbe39532db1ee2ea5
gouri <gouri257@gmail.com>
2014-02-23 21:00:35 +0000

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skyscrapper::Application.config.secret_key_base = '4c5715b847445f207b48c31c293e8ee5d22c79d997e58779c478683c5a3c7dd4d42eb58059f995760266b68f8d0938ad35302e9d5709201c1a4d60db3abe1888'
